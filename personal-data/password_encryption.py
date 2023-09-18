c6cddde228c3a2d6ac18ac971ffc3a1661b973a9
KariukiAntony <antonygichoya9@gmail.com>
2023-09-18 20:45:47 +0300

    work on hashing user password

--- personal-data/password_encryption.py
+++ personal-data/password_encryption.py
@@ -1,3 +1,16 @@
 """ A module for hashing passwords"""
 
 import bcrypt
+
+
+def generate_password_hash(password: str) -> bytes:
+
+          salt = bcrypt.gensalt()
+          encoded_password = password.encode("utf-8")
+          return bcrypt.hashpw(encoded_password, salt)
+
+
+def validate_user_password(user_password: str, hashed_password: bytes) -> bool:
+        
+        encoded_user_password = user_password.encode("utf-8")
+        return bcrypt.checkpw(encoded_user_password, hashed_password)
